4e82b4bbfb33c4f618e46267c5481659ea4e7790
level7webdev <6236517+level7webdev@users.noreply.github.com>
2020-11-04 11:09:34 +0000

    Fixes ActionMailer base smtp settings

--- config/environment.rb
+++ config/environment.rb
@@ -5,10 +5,10 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :port           => Rails.application.credentials.Mailgun[:MAILGUN_SMTP_PORT],
-  :address        => Rails.application.credentials.Mailgun[:MAILGUN_SMTP_SERVER],
-  :user_name      => Rails.application.credentials.Mailgun[:MAILGUN_SMTP_LOGIN],
-  :password       => Rails.application.credentials.Mailgun[:MAILGUN_SMTP_PASSWORD],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
   :domain         => 'l7wd-photo-app.heroku.com',
   :authentication => :plain,
 }
