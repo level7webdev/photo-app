08dc22fb1165f7b3c0fd074e7bf820c96d33eb3d
level7webdev <6236517+level7webdev@users.noreply.github.com>
2020-11-04 11:02:07 +0000

    Prepare ActionMailer for Mailgun in production

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :port           => Rails.application.credentials.Mailgun[:MAILGUN_SMTP_PORT],
+  :address        => Rails.application.credentials.Mailgun[:MAILGUN_SMTP_SERVER],
+  :user_name      => Rails.application.credentials.Mailgun[:MAILGUN_SMTP_LOGIN],
+  :password       => Rails.application.credentials.Mailgun[:MAILGUN_SMTP_PASSWORD],
+  :domain         => 'l7wd-photo-app.heroku.com',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
